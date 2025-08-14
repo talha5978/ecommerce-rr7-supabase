bdf99fe363a1201789ed084fdc5dbdb065ca0736
Talha <muhammadtalha13457@gmail.com>
2025-08-14 10:17:28 -0700

    Remove memo hook from data table component to fix no rows show bug when new rows are added

--- admin/app/components/Table/data-table.tsx
+++ admin/app/components/Table/data-table.tsx
@@ -54,180 +54,175 @@ export interface DataTableSkeletonProps {
 	columns: ColumnDef<any>[];
 }
 
-export const DataTable = memo(
-	({
-		table,
-		onPageChange,
-		onPageSizeChange,
-		pageSize,
-		total,
-		customEmptyMessage,
-		cellClassName = "**:data-[slot=table-cell]:last:bg-background",
-		headerClassName = "bg-muted",
-	}: DataTableProps) => {
-		if (!table) {
-			return (
-				<div>
-					<p className="text-center text-muted-foreground">
-						Table not initialized. Please check the data source.
-					</p>
-				</div>
-			);
-		}
-
-		const PAGE_VALUES = [10, 20, 30, 40, 50];
-
+export const DataTable = ({
+	table,
+	onPageChange,
+	onPageSizeChange,
+	pageSize,
+	total,
+	customEmptyMessage,
+	cellClassName = "**:data-[slot=table-cell]:last:bg-background",
+	headerClassName = "bg-muted",
+}: DataTableProps) => {
+	if (!table) {
 		return (
-			<section>
-				<TableComponent>
-					<TableHeader className={`sticky top-0 z-10`}>
-						{table.getHeaderGroups().map((headerGroup) => (
-							<TableRow
-								key={headerGroup.id}
-								className="[&>*]:whitespace-nowrap sticky top-0 bg-background after:content-[''] after:inset-x-0 after:h-px after:bg-border after:absolute after:bottom-0"
-							>
-								{headerGroup.headers.map((header, index) => {
-									const isFirst = index === 0;
-									const isLast = index === headerGroup.headers.length - 1;
+			<div>
+				<p className="text-center text-muted-foreground">
+					Table not initialized. Please check the data source.
+				</p>
+			</div>
+		);
+	}
 
-									return (
-										<TableHead
-											key={header.id}
-											className={`
+	const PAGE_VALUES = [10, 20, 30, 40, 50];
+
+	return (
+		<section>
+			<TableComponent>
+				<TableHeader className={`sticky top-0 z-10`}>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<TableRow
+							key={headerGroup.id}
+							className="[&>*]:whitespace-nowrap sticky top-0 bg-background after:content-[''] after:inset-x-0 after:h-px after:bg-border after:absolute after:bottom-0"
+						>
+							{headerGroup.headers.map((header, index) => {
+								const isFirst = index === 0;
+								const isLast = index === headerGroup.headers.length - 1;
+
+								return (
+									<TableHead
+										key={header.id}
+										className={`
 											${cn(headerClassName)}
 											${isFirst && "rounded-tl-lg"}
 											${isLast && "rounded-tr-lg"}`}
-										>
-											{header.isPlaceholder
-												? null
-												: flexRender(
-														header.column.columnDef.header,
-														header.getContext(),
-													)}
-										</TableHead>
-									);
-								})}
-							</TableRow>
-						))}
-					</TableHeader>
-					<TableBody
-						className={`**:data-[slot=table-cell]:first:w-8 **:data-[slot=table-cell]:last:sticky **:data-[slot=table-cell]:last:right-0 **:data-[slot=table-cell]:last:z-10 ${cellClassName}`}
-					>
-						{/* <AnimatePresence> */}
-						{table.getRowModel().rows?.length > 0 ? (
-							table.getRowModel().rows.map((row) => (
-								<motion.tr
-									key={row.id}
-									id={row.id}
-									initial={{ opacity: 0 }}
-									animate={{ opacity: 1 }}
-									transition={{ duration: 0.4, ease: "easeOut" }}
-									data-state={row.getIsSelected() && "selected"}
-									data-slot="table-row"
-									className="hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors"
-								>
-									{row.getVisibleCells().map((cell) => (
-										<TableCell key={cell.id}>
-											{flexRender(cell.column.columnDef.cell, cell.getContext())}
-										</TableCell>
-									))}
-								</motion.tr>
-							))
-						) : (
-							<TableRow>
-								<TableCell
-									colSpan={table.getAllColumns().length}
-									className="h-36 text-center select-none text-muted-foreground"
-								>
-									<p className="text-sm">{customEmptyMessage ?? "No results found"}</p>
-								</TableCell>
-							</TableRow>
-						)}
-						{/* </AnimatePresence> */}
-					</TableBody>
-				</TableComponent>
-				<div className="mt-4">
-					{total && (
-						<div className="px-4">
-							<p>
-								({total}) record{total ? (total === 1 ? "" : "s") : "s"} found
-							</p>
-						</div>
+									>
+										{header.isPlaceholder
+											? null
+											: flexRender(header.column.columnDef.header, header.getContext())}
+									</TableHead>
+								);
+							})}
+						</TableRow>
+					))}
+				</TableHeader>
+				<TableBody
+					className={`**:data-[slot=table-cell]:first:w-8 **:data-[slot=table-cell]:last:sticky **:data-[slot=table-cell]:last:right-0 **:data-[slot=table-cell]:last:z-10 ${cellClassName}`}
+				>
+					{/* <AnimatePresence> */}
+					{table.getRowModel().rows?.length > 0 ? (
+						table.getRowModel().rows.map((row) => (
+							<motion.tr
+								key={row.id}
+								id={row.id}
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1 }}
+								transition={{ duration: 0.4, ease: "easeOut" }}
+								data-state={row.getIsSelected() && "selected"}
+								data-slot="table-row"
+								className="hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors"
+							>
+								{row.getVisibleCells().map((cell) => (
+									<TableCell key={cell.id}>
+										{flexRender(cell.column.columnDef.cell, cell.getContext())}
+									</TableCell>
+								))}
+							</motion.tr>
+						))
+					) : (
+						<TableRow>
+							<TableCell
+								colSpan={table.getAllColumns().length}
+								className="h-36 text-center select-none text-muted-foreground"
+							>
+								<p className="text-sm">{customEmptyMessage ?? "No results found"}</p>
+							</TableCell>
+						</TableRow>
 					)}
-					{onPageSizeChange && onPageChange && pageSize ? (
-						<div className="mt-4 flex w-full items-center gap-8 justify-between px-4">
-							<div className="hidden items-center gap-2 sm:flex">
-								<Label htmlFor="rows-per-page" className="text-sm font-medium">
-									Rows per page
-								</Label>
-								<Select
-									value={`${pageSize}`}
-									onValueChange={(value) => onPageSizeChange(Number(value))}
-								>
-									<SelectTrigger size="sm" className="w-20" id="rows-per-page">
-										<SelectValue placeholder={table.getState().pagination.pageSize} />
-									</SelectTrigger>
-									<SelectContent side="top" defaultValue={`${pageSize}`}>
-										{PAGE_VALUES.map((size) => (
-											<SelectItem key={size} value={`${size}`}>
-												{size}
-											</SelectItem>
-										))}
-									</SelectContent>
-								</Select>
-							</div>
-							<div className="flex w-fit items-center justify-center text-sm font-medium">
-								Page {table.getState().pagination.pageIndex + 1} of{" "}
-								{table.getPageCount() || 1}
-							</div>
-							<div className="ml-auto flex items-center gap-2 sm:ml-0">
-								<Button
-									variant="outline"
-									className="hidden h-8 w-8 p-0 sm:flex"
-									onClick={() => onPageChange(0)}
-									disabled={!table.getCanPreviousPage()}
-								>
-									<span className="sr-only">Go to first page</span>
-									<IconChevronsLeft />
-								</Button>
-								<Button
-									variant="outline"
-									className="size-8"
-									size="icon"
-									onClick={() => onPageChange(table.getState().pagination.pageIndex - 1)}
-									disabled={!table.getCanPreviousPage()}
-								>
-									<span className="sr-only">Go to previous page</span>
-									<IconChevronLeft />
-								</Button>
-								<Button
-									variant="outline"
-									className="size-8"
-									size="icon"
-									onClick={() => onPageChange(table.getState().pagination.pageIndex + 1)}
-									disabled={!table.getCanNextPage()}
-								>
-									<span className="sr-only">Go to next page</span>
-									<IconChevronRight />
-								</Button>
-								<Button
-									variant="outline"
-									className="hidden size-8 sm:flex"
-									size="icon"
-									onClick={() => onPageChange(table.getPageCount() - 1)}
-									disabled={!table.getCanNextPage()}
-								>
-									<span className="sr-only">Go to last page</span>
-									<IconChevronsRight />
-								</Button>
-							</div>
+					{/* </AnimatePresence> */}
+				</TableBody>
+			</TableComponent>
+			<div className="mt-4">
+				{total && (
+					<div className="px-4">
+						<p>
+							({total}) record{total ? (total === 1 ? "" : "s") : "s"} found
+						</p>
+					</div>
+				)}
+				{onPageSizeChange && onPageChange && pageSize ? (
+					<div className="mt-4 flex w-full items-center gap-8 justify-between px-4">
+						<div className="hidden items-center gap-2 sm:flex">
+							<Label htmlFor="rows-per-page" className="text-sm font-medium">
+								Rows per page
+							</Label>
+							<Select
+								value={`${pageSize}`}
+								onValueChange={(value) => onPageSizeChange(Number(value))}
+							>
+								<SelectTrigger size="sm" className="w-20" id="rows-per-page">
+									<SelectValue placeholder={table.getState().pagination.pageSize} />
+								</SelectTrigger>
+								<SelectContent side="top" defaultValue={`${pageSize}`}>
+									{PAGE_VALUES.map((size) => (
+										<SelectItem key={size} value={`${size}`}>
+											{size}
+										</SelectItem>
+									))}
+								</SelectContent>
+							</Select>
 						</div>
-					) : null}
-				</div>
-			</section>
-		);
-	},
-);
+						<div className="flex w-fit items-center justify-center text-sm font-medium">
+							Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount() || 1}
+						</div>
+						<div className="ml-auto flex items-center gap-2 sm:ml-0">
+							<Button
+								variant="outline"
+								className="hidden h-8 w-8 p-0 sm:flex"
+								onClick={() => onPageChange(0)}
+								disabled={!table.getCanPreviousPage()}
+							>
+								<span className="sr-only">Go to first page</span>
+								<IconChevronsLeft />
+							</Button>
+							<Button
+								variant="outline"
+								className="size-8"
+								size="icon"
+								onClick={() => onPageChange(table.getState().pagination.pageIndex - 1)}
+								disabled={!table.getCanPreviousPage()}
+							>
+								<span className="sr-only">Go to previous page</span>
+								<IconChevronLeft />
+							</Button>
+							<Button
+								variant="outline"
+								className="size-8"
+								size="icon"
+								onClick={() => onPageChange(table.getState().pagination.pageIndex + 1)}
+								disabled={!table.getCanNextPage()}
+							>
+								<span className="sr-only">Go to next page</span>
+								<IconChevronRight />
+							</Button>
+							<Button
+								variant="outline"
+								className="hidden size-8 sm:flex"
+								size="icon"
+								onClick={() => onPageChange(table.getPageCount() - 1)}
+								disabled={!table.getCanNextPage()}
+							>
+								<span className="sr-only">Go to last page</span>
+								<IconChevronsRight />
+							</Button>
+						</div>
+					</div>
+				) : null}
+			</div>
+		</section>
+	);
+};
+
 
 export const DataTableSkeleton = memo(function DataTableSkeleton({
 	noOfSkeletons = 8,
