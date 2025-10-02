799d2146564a865ce1b2c77c83d3a3509e8f8d15
Muhammad Talha <muhammadtalha13457@gmail.com>
2025-10-02 20:32:59 +0500

    Remove unnecessary page title in the header of admin panel

--- admin/app/components/Nav/site-header.tsx
+++ admin/app/components/Nav/site-header.tsx
@@ -1,19 +1,13 @@
-import { Separator } from "~/components/ui/separator";
 import { SidebarTrigger } from "~/components/ui/sidebar";
 import { ThemeToggleButton } from "~/components/Theme/theme-toggle";
-import { usePageTitle } from "~/hooks/use-page-title";
 import { UserButton } from "~/components/Nav/nav-user";
 import GlobalSearch from "~/components/Nav/global-search";
 
 export function SiteHeader() {
-	const pageTitle = usePageTitle();
-
 	return (
 		<header className="flex h-[var(--header-spacing)] shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-[var(--header-spacing)]">
 			<div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
 				<SidebarTrigger className="-ml-1 cursor-pointer" />
-				<Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
-				<span className="text-base font-medium">{pageTitle}</span>
 				<div className="ml-auto flex items-center gap-2">
 					<GlobalSearch />
 					<ThemeToggleButton />
