ecf53c0179231ec723083496ed74ccd4c20a188e
Muhammad Talha <muhammadtalha13457@gmail.com>
2026-06-22 15:15:06 +0500

    Remove rollup-plugin-visualizer package usage in front-panel

--- front-panel/vite.config.ts
+++ front-panel/vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { visualizer } from "rollup-plugin-visualizer";
 import Inspect from "vite-plugin-inspect";
 import viteCompression from "vite-plugin-compression";
 
@@ -21,7 +20,6 @@ export default defineConfig(({ mode }) => {
 				],
 			}),
 			Inspect(),
-			visualizer({ open: true }),
 			viteCompression({
 				verbose: true,
 				disable: false,
